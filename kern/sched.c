7e1073cc3439bb6c5a5ecb66617148eac173e08a
daniel-demi <dany.demi96@gamil.com>
2022-04-12 21:37:43 -0700

    ex6

--- kern/sched.c
+++ kern/sched.c
@@ -29,9 +29,45 @@ sched_yield(void)
 	// below to halt the cpu.
 
 	// LAB 4: Your code here.
-
-	// sched_halt never returns
+	//~ if(curenv) {
+		//~ cprintf("dbg: %s:%d\n",__FILE__,__LINE__);
+		//~ int curr_idx = ENVX(curenv->env_id);
+		//~ int i;
+		//~ for(i = 0; i < NENV; i++) {
+			//~ int idx = (curr_idx + i + 1) % NENV;
+			//~ if(envs[i].env_status == ENV_RUNNABLE) {
+				//~ env_run(&envs[idx]);
+				//~ break;
+			//~ }
+		//~ }
+		//~ if (curenv->env_status == ENV_RUNNING) {
+			//~ env_run(curenv);
+		//~ }
+	//~ }
+	//~ // sched_halt never returns
+	//~ sched_halt();
+	//~ panic("shced_yield: returned");
+	
+	int begin = curenv ? ENVX(curenv->env_id) + 1 : 0;
+	int index = begin;
+	bool found = false;
+	int i;
+	for (i = 0; i < NENV; i++) {
+	index = (begin + i) % NENV;
+	if (envs[index].env_status == ENV_RUNNABLE) {
+	found = true;
+	break;
+	}
+	}
+	
+	if (found) {
+	env_run(&envs[index]);
+	} else if (curenv && curenv->env_status == ENV_RUNNING) {
+	env_run(curenv);
+	} else {
 	sched_halt();
+	}
+	panic("sched_yield attempted to return");
 }
 
 // Halt this CPU when there is nothing to do. Wait until the
@@ -40,6 +76,7 @@ sched_yield(void)
 void
 sched_halt(void)
 {
+	//~ cprintf("dbg: sched_halt start\n");
 	int i;
 
 	// For debugging and testing purposes, if there are no runnable
@@ -50,23 +87,28 @@ sched_halt(void)
 		     envs[i].env_status == ENV_DYING))
 			break;
 	}
+	//~ cprintf("%s:%d\n",__FILE__,__LINE__);
 	if (i == NENV) {
 		cprintf("No runnable environments in the system!\n");
 		while (1)
 			monitor(NULL);
 	}
+	//~ cprintf("%s:%d\n",__FILE__,__LINE__);
 
 	// Mark that no environment is running on this CPU
 	curenv = NULL;
 	lcr3(PADDR(kern_pgdir));
+	//~ cprintf("%s:/%d\n",__FILE__,__LINE__);
 
 	// Mark that this CPU is in the HALT state, so that when
 	// timer interupts come in, we know we should re-acquire the
 	// big kernel lock
 	xchg(&thiscpu->cpu_status, CPU_HALTED);
+	//~ cprintf("%s:%d\n",__FILE__,__LINE__);
 
 	// Release the big kernel lock as if we were "leaving" the kernel
 	unlock_kernel();
+	//~ cprintf("%s:%d\n",__FILE__,__LINE__);
 
 	// Reset stack pointer, enable interrupts and then halt.
 	asm volatile (
@@ -79,5 +121,6 @@ sched_halt(void)
 		"hlt\n"
 		"jmp 1b\n"
 	: : "a" (thiscpu->cpu_ts.ts_esp0));
+	//~ cprintf("dbg: sched_halt end\n");
 }
 
